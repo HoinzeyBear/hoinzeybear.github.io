3970c49046a0aa5c71929c904bd86327d556c1c9
dhoyne <darrenhoyne@gmail.com>
2021-04-24 15:17:21 +0100

    post for filtering arrays and lists

---
+++ site/script.js
@@ -0,0 +1,32 @@
+var codeBlockWrappers = document.getElementsByClassName("code-block-wrapper")
+for(var i=0; i < codeBlockWrappers.length; i++){
+    var wrapper = codeBlockWrappers[i]
+    var headers = wrapper.querySelectorAll(".cb-header")
+    var blocks = wrapper.querySelectorAll(".code-block")
+
+    headers[0].className += " selected"
+    blocks[0].className += " selected"
+
+    var onClick = function(pos, headers, blocks) {
+        return function() {
+            var blockToHide
+            var headerToHide
+            for(var k=0; k < blocks.length; k++){
+                if(String(blocks[k].className).includes("selected")) {
+                    blockToHide = blocks[k]
+                    headerToHide = headers[k]
+                }
+            }
+            if(blockToHide) {
+                blockToHide.className = String(blockToHide.className).replace(" selected","")
+                blocks[pos].className += " selected"
+
+                headerToHide.className = String(headerToHide.className).replace(" selected","")
+                headers[pos].className += " selected"
+            }
+        }
+    }
+    for(var t=0; t < headers.length; t++) {
+        headers[t].addEventListener("click", onClick(t, headers, blocks))
+    }
+}
